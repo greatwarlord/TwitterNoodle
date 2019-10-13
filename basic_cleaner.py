f7f61cd4fc757c7f1acbf0c3fa4c672238c71d23
crunchypi <alekscrank@gmail.com>
2019-10-13 16:25:05 +0200

    added punctuation cleaner

--- basic_cleaner.py
+++ basic_cleaner.py
@@ -3,6 +3,7 @@ from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from nltk.tokenize.treebank import TreebankWordDetokenizer 
 import re
+import string
 
 
 
@@ -14,7 +15,7 @@ class BasicCleaner():
 
     def __init__(self, _text, _auto):
         self.text_raw = _text
-        self.text_processed = "dank"
+        self.text_processed = ""
         if _auto:
             self.autocleaner()
 
@@ -34,7 +35,8 @@ class BasicCleaner():
     def autocleaner(self):
         #self.text_processed = self.text_raw
         no_link = self.clean_links(self.text_raw)
-        tokens = self.tokenise(no_link)
+        no_punct = self.clean_punctuation(no_link)
+        tokens = self.tokenise(no_punct)
         wo_stop = self.clean_stopwords(tokens)
         de_tokens = self.detokenise(wo_stop)
 
@@ -54,8 +56,10 @@ class BasicCleaner():
 
     def clean_numbers(self):
         pass
-    def clean_punctuation(self):
-        pass
+
+    def clean_punctuation(self, text_in):
+        return text_in.translate(str.maketrans('', '', string.punctuation))
+
     def clean_links(self, text_in):
         return re.sub(r"[a-z]*[:.]+\S+", "",text_in)
 

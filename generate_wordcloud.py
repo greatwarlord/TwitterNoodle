b27676312d9db0ecf16ddd5ce5b77f2e057f8b8b
crunchypi <alekscrank@gmail.com>
2019-10-20 21:28:44 +0200

    added sentiment range

--- generate_wordcloud.py
+++ generate_wordcloud.py
@@ -4,12 +4,12 @@ import pandas as pd
 from tweet_feed import Feed
 from basic_cleaner import BasicCleaner
 import data_object
-  
 
 
 
 
-file_path = "../DataCollection/191020-18_19_45--191020-18_19_47" 
+file_path = "../DataCollection/191020-20_39_57--191020-20_40_07" 
+sentiment_range = [float(-1), float(-0.5)]
 
 
 
@@ -17,13 +17,12 @@ def get_long_tweet_string():
     feed = Feed()
     queue_stream = feed.disk_get_tweet_queue(file_path)
     data_objects = [data_object.get_dataobj_converted(tweet) for tweet in queue_stream]
-    #cleaned_data_objects = [BasicCleaner.autocleaner(obj, False) for obj in data_objects]
-    for obj in data_objects: BasicCleaner.autocleaner(obj, False)
-    long_string = [obj.text for obj in data_objects]
+    for obj in data_objects: BasicCleaner.autocleaner(obj,sentiment_range, True)
+    long_string = [obj.text*(obj.valid_sentiment_range) for obj in data_objects]
     return " ".join(long_string)
 
 
-  
+
 WC = WordCloud(width = 800, height = 800, 
                 background_color ='white',  
                 min_font_size = 10).generate(get_long_tweet_string()) 

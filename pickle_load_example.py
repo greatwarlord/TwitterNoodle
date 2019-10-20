0b8246969d6dbc0ba02c88382aa2a52608b70212
crunchypi <alekscrank@gmail.com>
2019-10-20 18:33:11 +0200

    removed unused module and cleaned up the file

--- pickle_load_example.py
+++ pickle_load_example.py
@@ -1,9 +1,8 @@
-import pickle
 from tweet_feed import Feed
 
 
 
-file_path = "../DataCollection/191013-20_19_45--191013-20_19_50"
+file_path = "../DataCollection/191019-17_45_16--191019-17_45_18"
 feed = Feed()
 queue_stream = feed.disk_get_tweet_queue(file_path)
 
@@ -12,10 +11,4 @@ queue_stream = feed.disk_get_tweet_queue(file_path)
 for item in queue_stream:
     print(item.text)
 
-
-
 print("end of program, terminating..")
-
-
-
-l = [x.text for x in queue_stream]

bbde4ae7cc6b47d4b1a6ac7d6f3040627e660bfc
crunchypi <alekscrank@gmail.com>
2019-11-08 20:56:06 +0100

    added functionality for creating csv

--- generate_wordcloud.py
+++ generate_wordcloud.py
@@ -23,17 +23,44 @@ def get_long_tweet_string():
     long_string = [obj.text*(obj.valid_sentiment_range) for obj in data_objects]
     return " ".join(long_string)
 
+def get_long_tweet_objects():
+    feed = Feed()
+    queue_stream = feed.disk_get_tweet_queue(file_path)
+    data_objects = [data_object.get_dataobj_converted(tweet) for tweet in queue_stream]
+    for obj in data_objects: BasicCleaner.autocleaner(obj,sentiment_range, True)
+    return data_objects
+
+def generate_wordcloud():
+    WC = WordCloud(width = 800, height = 800, 
+                    background_color ='white',  
+                    min_font_size = 10).generate(get_long_tweet_string()) 
+    
+    # plot WC                       
+    plt.figure(figsize = (8, 8), facecolor = None) 
+    plt.imshow(WC) 
+    plt.axis("off") 
+    plt.tight_layout(pad = 0) 
+
+    plt.show() 
+    
+        # self.alphatags = []
+
+import csv
+def write_csv(_filename):
+    with open(_filename, 'w', newline='') as csvfile:
+        obj_writer = csv.writer(csvfile, delimiter=' ',
+                                quotechar='|', quoting=csv.QUOTE_MINIMAL)
+
+        obj_list = get_long_tweet_objects()
+        obj_writer.writerow(["name"] + ["txt"] + ["coord"] + ["places"] + ["hashtags"] + ["alphatags"] + ["sentiment"])
+        for obj in obj_list:
+            obj_writer.writerow([obj.name] + 
+                                [obj.text] + 
+                                [obj.coordinates] +
+                                [obj.place] + 
+                                [obj.hashtags] +
+                                [obj.alphatags] + 
+                                [obj.valid_sentiment_range])
 
 
-WC = WordCloud(width = 800, height = 800, 
-                background_color ='white',  
-                min_font_size = 10).generate(get_long_tweet_string()) 
-  
-# plot WC                       
-plt.figure(figsize = (8, 8), facecolor = None) 
-plt.imshow(WC) 
-plt.axis("off") 
-plt.tight_layout(pad = 0) 
-
-
-plt.show() 
+write_csv("dank.csv")

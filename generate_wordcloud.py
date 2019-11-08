f51203a407dcd37678c4d07a77530655ebeb73bf
crunchypi <alekscrank@gmail.com>
2019-11-08 21:21:06 +0100

    simplified a bit

--- generate_wordcloud.py
+++ generate_wordcloud.py
@@ -13,23 +13,16 @@ import csv
 file_path = "../DataCollection/191107-16_38_54--191107-16_38_59" 
 sentiment_range = [float(-1), float(1)]
 
-
-# // AA: Helper for creating continious string for wordclouds.
-# //        Might need a revision to remove printout to std
-def get_long_tweet_string():
-    feed = Feed()
-    queue_stream = feed.disk_get_tweet_queue(file_path)
-    data_objects = [data_object.get_dataobj_converted(tweet) for tweet in queue_stream]
-    for obj in data_objects: BasicCleaner.autocleaner(obj,sentiment_range, True)
-    long_string = [obj.text*(obj.valid_sentiment_range) for obj in data_objects]
-    return " ".join(long_string)
-
 def get_long_tweet_objects():
     feed = Feed()
     queue_stream = feed.disk_get_tweet_queue(file_path)
     data_objects = [data_object.get_dataobj_converted(tweet) for tweet in queue_stream]
     for obj in data_objects: BasicCleaner.autocleaner(obj,sentiment_range, True)
     return data_objects
+
+def get_long_tweet_string():
+    long_string = [obj.text*(obj.valid_sentiment_range) for obj in get_long_tweet_objects()]
+    return " ".join(long_string)
 
 def generate_wordcloud():
     WC = WordCloud(width = 800, height = 800, 
@@ -41,14 +34,12 @@ def generate_wordcloud():
     plt.imshow(WC) 
     plt.axis("off") 
     plt.tight_layout(pad = 0) 
-
     plt.show() 
-    
 
 def write_csv(_filename):
     with open(_filename, 'w', newline='') as csvfile:
-        obj_writer = csv.writer(csvfile, delimiter=' ',
-                                quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        obj_writer = csv.writer(csvfile, delimiter=',',
+                                quotechar=' ', quoting=csv.QUOTE_MINIMAL)
 
         obj_list = get_long_tweet_objects()
         obj_writer.writerow(["name"] + ["txt"] + ["coord"] + ["places"] + ["hashtags"] + ["alphatags"] + ["sentiment"])
@@ -62,4 +53,3 @@ def write_csv(_filename):
                                 [obj.valid_sentiment_range])
 
 
-write_csv("dank.csv")

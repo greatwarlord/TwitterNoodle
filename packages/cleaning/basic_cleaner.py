de0a9c291adc0572223af8a032c859b56f6a1357
crunchypi <alekscrank@gmail.com>
2020-01-12 20:22:04 +0100

    Added better punctiuation removal

--- packages/cleaning/basic_cleaner.py
+++ packages/cleaning/basic_cleaner.py
@@ -47,12 +47,23 @@ class BasicCleaner():
         self.detokenise(_data_obj)
 
         self.clean_punctuation(_data_obj)
+        # // next line can probably be removed now (010220) because
+        # // I added a change in clean_punctuation which removes everything 
+        # // non-alpha.
         self.clean_numbers(_data_obj)
    
+        self.remove_duplica_words(_data_obj)
         self.set_sentiment(_data_obj, _sentiment_range)
 
         if _verbosity:
             self.print_comparison(_data_obj, text_raw)
+
+    @staticmethod
+    def remove_duplica_words(_data_obj):
+        words = _data_obj.text.split()
+        non_dup = set(words)
+        _data_obj.text = " ".join(non_dup)
+
  
     @staticmethod
     def tokenise(_data_obj):
@@ -78,7 +89,18 @@ class BasicCleaner():
 
     @staticmethod
     def clean_punctuation(_data_obj):
-        _data_obj.text = _data_obj.text.translate(str.maketrans('', '', string.punctuation))
+        # Removed on 010120.
+        #_data_obj.text = _data_obj.text.translate(str.maketrans('', '', string.punctuation))
+        # Added on 010120 for testing, will revert by 010220 if it's bust.
+        new_string = ""
+        str_split = _data_obj.text.split()
+        for chunk in str_split:
+            tmp = ""
+            for char in chunk:
+                if char.isalpha():
+                    tmp += char
+            new_string += f" {tmp}" # // could check if tmp only contains space
+        _data_obj.text = new_string
 
     @staticmethod
     def clean_links(_data_obj):
